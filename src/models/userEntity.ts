a5b097d78c19f139e3e0ceb5d9a46196d3b87395
Ahmed <ahmdhusam7@gmail.com>
2022-07-31 17:45:33 +0200

    edit user schema

--- src/models/userEntity.ts
+++ src/models/userEntity.ts
@@ -11,6 +11,11 @@ import {
 import { Follow } from './followEntity';
 import { Tweet } from './tweetEntity';
 
+export enum Gender {
+  Male = 'male',
+  Female = 'female'
+}
+
 @Check(`"birth_date" < (CURRENT_DATE - interval '18 year -1 day')`)
 @Entity({ name: 'users' })
 export class User extends BaseEntity {
@@ -26,23 +31,29 @@ export class User extends BaseEntity {
   @Column({ unique: true, length: 50 })
   email: string;
 
-  @Column({ length: 61 })
-  password: string;
-
   @Column({ length: 100, default: '' })
   bio: string;
 
   @Column({ name: 'birth_date', type: 'date' })
   birthDate: Date;
 
-  @Column({ default: '' })
+  @Column({ type: 'enum', enum: Gender })
+  gender: Gender;
+
+  @Column({ default: '/images/avatar.png' })
   avatar: string;
+
+  @Column({ nullable: true })
+  header: string;
 
   @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
 
   @UpdateDateColumn({ name: 'updated_at' })
   updatedAt: Date;
+
+  @Column({ length: 61 })
+  password: string;
 
   @OneToMany(() => Tweet, tweet => tweet.creator)
   tweets: Tweet[];
