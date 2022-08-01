f0949487d3caffbc5f1ede6b2b650b91bdc47305
Ahmed <ahmdhusam7@gmail.com>
2022-08-01 19:23:03 +0200

    configure users & tweets parser

--- src/libs/tweetParser.ts
+++ src/libs/tweetParser.ts
@@ -1,20 +1,25 @@
-import type { GetTweets } from '../types';
-
-import { TweetModel } from '../models';
-
+import type { GetTweets, ITweet } from '../types';
+import { In } from 'typeorm';
+import { Tweet } from '../models';
 import { getUser } from '.';
 
-export const getTweets: GetTweets = async (tweetsId: string[]) => {
-    const tweets = await TweetModel.find({ _id: { $in: tweetsId } });
-
-    return tweets.map(parseTweet);
+export const parseTweet = (tweet: Tweet): ITweet => {
+  return {
+    ...tweet,
+    creator: getUser.bind(null, tweet.creator.id)
+  };
 };
 
-export const parseTweet = (tweet: any) => {
-    return {
-        ...tweet._doc,
-        id: tweet.id,
-        replys: getTweets.bind(null, tweet._doc.replys),
-        creator: getUser.bind(null, tweet._doc.creator)
-    };
+export const getTweets: GetTweets = async (tweetsId: string[]) => {
+  const tweets = await Tweet.find({
+    where: { id: In(tweetsId) },
+    relations: ['creator'],
+    select: {
+      creator: {
+        id: true
+      }
+    }
+  });
+
+  return tweets.map(parseTweet);
 };
--- src/libs/userParser.ts
+++ src/libs/userParser.ts
@@ -1,28 +1,75 @@
-import type { GetUsers, GetUser } from '../types';
-
-import { UserModel } from '../models';
-
+import type { GetUsers, GetUser, IUser } from '../types';
+import { In } from 'typeorm';
+import { User } from '../models';
 import { getTweets } from '.';
 
-export function parseUser(user: any) {
-    return {
-        ...user._doc,
-        id: user.id,
-        tweets: getTweets.bind(null, user._doc.tweets),
-        following: getUsers.bind(null, user._doc.following),
-        followers: getUsers.bind(null, user._doc.followers)
-    };
+export function parseUser(user: User): IUser {
+  return {
+    ...user,
+    tweets: getTweets.bind(
+      null,
+      user.tweets.slice(0, 20).map(tweet => tweet.id)
+    ),
+    followings: getUsers.bind(
+      null,
+      user.followings.slice(0, 20).map(follow => follow.following.toString())
+    ),
+    followers: getUsers.bind(
+      null,
+      user.followers.slice(0, 20).map(follow => follow.follower.toString())
+    )
+  };
 }
 
 export const getUser: GetUser = async (userId: string) => {
-    const user = await UserModel.findById(userId).select('-password');
-    if (!user) {
-        throw new Error('User Not Found');
+  const user = await User.findOne({
+    where: {
+      id: userId
+    },
+    relations: ['tweets', 'followings', 'followers'],
+    select: {
+      password: false,
+      tweets: {
+        id: true
+      },
+      followings: {
+        following: {
+          id: true
+        }
+      },
+      followers: {
+        follower: {
+          id: true
+        }
+      }
     }
-    return parseUser(user);
+  });
+
+  return parseUser(user);
 };
 
 const getUsers: GetUsers = async (usersId: string[]) => {
-    const users = await UserModel.find({ _id: { $in: usersId } }).select('-password');
-    return users.map(parseUser);
+  const users = await User.find({
+    where: {
+      id: In(usersId)
+    },
+    relations: ['tweets', 'followings', 'followers'],
+    select: {
+      password: false,
+      tweets: {
+        id: true
+      },
+      followings: {
+        following: {
+          id: true
+        }
+      },
+      followers: {
+        follower: {
+          id: true
+        }
+      }
+    }
+  });
+  return users.map(parseUser);
 };
