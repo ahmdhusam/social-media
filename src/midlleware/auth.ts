6b14a50470a0778f010eee13c5e661323c338a46
Ahmed <ahmdhusam7@gmail.com>
2022-08-01 19:30:41 +0200

    configure auth midlleware

--- src/midlleware/auth.ts
+++ src/midlleware/auth.ts
@@ -1,32 +1,30 @@
 import type { Request, Response, NextFunction } from 'express';
-import type { Req } from '../types';
 
 import jwt from 'jsonwebtoken';
 
-import { UserModel } from '../models';
+import { User } from '../models';
 
 interface ITokenData {
-    userId: string;
-    email: string;
+  userId: string;
+  email: string;
 }
 
-export default async (request: Request, _: Response, next: NextFunction) => {
-    const req: Req = request as Req;
-    const token = req.headers.authorization?.split(' ')[1];
-    if (!token) {
-        req.User = null;
-        return next();
-    }
+export default async (req: Request, _: Response, next: NextFunction): Promise<void> => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) {
+    req.User = null;
+    return next();
+  }
 
-    try {
-        const tokenData = <ITokenData>jwt.verify(token, <string>process.env.SECRET_KEY);
-        if (!('userId' in tokenData && 'email' in tokenData)) throw new Error('');
+  try {
+    const tokenData = <ITokenData>jwt.verify(token, <string>process.env.SECRET_KEY);
+    if (!('userId' in tokenData && 'email' in tokenData)) throw new Error('');
 
-        const user = await UserModel.findOne({ _id: tokenData.userId, email: tokenData.email });
-        req.User = user;
-    } catch (err) {
-        req.User = null;
-    } finally {
-        next();
-    }
+    const user = await User.findOneBy({ id: tokenData.userId, email: tokenData.email });
+    req.User = user;
+  } catch (err) {
+    req.User = null;
+  } finally {
+    next();
+  }
 };
